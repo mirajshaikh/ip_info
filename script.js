0eab5a185fdd352a2ce9dcb0af98def5f4010440
miraj <mirajpc@gmail.com>
2020-01-31 01:16:05 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+  //api key https://api.ipgeolocation.io/ipgeo?apiKey=9ca8963aff474f50aa21560017ea3a7e&ip=1.1.1.1
+  //ip 2 https://api.ipgeolocation.io/ipgeo?apiKey=9ca8963aff474f50aa21560017ea3a7e&ip=
+     $(document).ready(function() {
+        $("#secound-countainer").hide();
+        $('#wait').hide();
+        $('#home').hide();
+
+
+        $(document).on({
+            ajaxStart: function(){ 
+                $('#wait').show();
+            },
+             ajaxStop: function(){ 
+                $('#wait').hide(); }    
+        });
+       
+        $("#getInfo2").click(function(){
+        $("#secound-countainer").show();
+        $("#buttonDiv").hide();
+    });
+        $('#back').click(function(){
+            $('#buttonDiv').show();
+            $('#secound-countainer').hide();
+    });
+        $("#getInfo").click(function(){
+             $.ajax({
+    url: "https://api.ipgeolocation.io/ipgeo?apiKey=9ca8963aff474f50aa21560017ea3a7e",
+    dataType: 'json',
+    success: function(results){
+        $('#ip').text("Your Ip Address Is : " + results.ip)
+        $('#country').text('Your Country Is ' + results.country_name)
+        $('#city').text('Your City Is ' + results.city)
+        $('#countryImg').attr('src', results.country_flag)
+        $('#currency').text('Your Currency Name Is ' + results.currency.name )
+        $('#currencyCode').text('Your currency Code Is '+ results.currency.code)
+        $('#currencySym').text('Your Currency Symbol is '+ results.currency.symbol)
+}
+        });
+             $('#buttonDiv').hide()
+             $('#home').show();
+
+        });
+        $("#getip").click(function(){
+            var ipaddress = $('#ipaddress').val();
+            $.ajax({
+                url: "https://api.ipgeolocation.io/ipgeo?apiKey=9ca8963aff474f50aa21560017ea3a7e&ip=" + ipaddress ,
+                dataType: 'json',
+                success: function(results){
+                    $('#ip').text("Your Ip Address Is : " + results.ip)
+                    $('#country').text('Your Country Is ' + results.country_name)
+                    $('#city').text('Your City Is ' + results.city)
+                    $('#countryImg').attr('src', results.country_flag)
+                    $('#currency').text('Your Currency Name Is ' + results.currency.name )
+                    $('#currencyCode').text('Your currency Code Is '+ results.currency.code)
+                    $('#currencySym').text('Your Currency Symbol is '+ results.currency.symbol)
+            }
+                    });
+            $('#secound-countainer').hide()
+            $('#home').show();
+
+        });
+
+
+     
+      }); 
